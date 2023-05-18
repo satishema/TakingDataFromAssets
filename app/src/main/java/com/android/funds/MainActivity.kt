326972233a487d1c2ab8cfea10244892b3b7343d
Satish <iamsatishema@gmail.com>
2023-05-18 14:38:48 +0530

    updated

--- app/src/main/java/com/android/funds/MainActivity.kt
+++ app/src/main/java/com/android/funds/MainActivity.kt
@@ -1,50 +1,1 @@
-package com.android.funds
-
-import android.os.Bundle
-import android.text.Html
-import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.text.HtmlCompat
-import androidx.databinding.DataBindingUtil
-import com.android.funds.databinding.ActivityMainBinding
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-
-class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
-    var funds: Funds? = null
-    var data : Data?  = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        getFunds()
-    }
-
-    private fun getFunds() {
-        val jsonFileString = Utils.getJsonDataFromAsset(applicationContext, "funds.json")
-        Log.i("data", jsonFileString!!)
-
-        val gson = Gson()
-        val fundsData = object : TypeToken<Funds>() {}.type
-
-        funds = gson.fromJson(jsonFileString, fundsData)
-        funds?.data?.forEachIndexed { idx, person ->
-            Log.i("data", "> Item $idx:\n$person")
-        }
-
-        funds?.data?.let {
-            var d = 0
-            for (data in it.withIndex()) {
-                if (data.value.date > 1668162810) {
-                    d += data.value.depositAmount
-                    binding.tvDeposit.text = HtmlCompat.fromHtml(
-                        "Deposit Amount: <b>$d</b>",
-                        HtmlCompat.FROM_HTML_MODE_LEGACY
-                    )
-                }
-            }
-        }
-    }
-
-}
+package com.android.fundsimport android.os.Bundleimport android.util.Logimport androidx.appcompat.app.AppCompatActivityimport androidx.core.text.HtmlCompatimport androidx.databinding.DataBindingUtilimport com.android.funds.databinding.ActivityMainBindingimport com.google.gson.Gsonimport com.google.gson.reflect.TypeTokenimport java.text.SimpleDateFormatimport java.util.*class MainActivity : AppCompatActivity() {    private var TAG = "MainActivity"    lateinit var binding: ActivityMainBinding    var funds: Funds? = null    var data : Data?  = null    override fun onCreate(savedInstanceState: Bundle?) {        super.onCreate(savedInstanceState)        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)        getFunds()        val a = Calendar.getInstance()        Log.d(TAG, "onCreate: $a")        val c = a.time        Log.d(TAG, "onCreate: $c")        val df = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())        val formattedDate: String = df.format(c)        Log.d(TAG, "onCreate: $formattedDate")    }    private fun getFunds() {        val jsonFileString = Utils.getJsonDataFromAsset(applicationContext, "funds.json")        Log.i("data", jsonFileString!!)        val gson = Gson()        val fundsData = object : TypeToken<Funds>() {}.type        funds = gson.fromJson(jsonFileString, fundsData)        funds?.data?.forEachIndexed { idx, person ->            Log.i("data", "> Item $idx:\n$person")        }        funds?.data?.let {            var d = 0            for (data in it.withIndex()) {                val df = SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault())                val expiry = Date(data.value.date.toLong() * 1000 )                val new = df.format(expiry)                println(new)                if (data.value.date > 1668162810) {                    d += data.value.depositAmount                    binding.tvDeposit.text = HtmlCompat.fromHtml(                        "Deposit Amount: <b>$d</b>",                        HtmlCompat.FROM_HTML_MODE_LEGACY                    )                }            }        }    }}
